37b3874305da142cfdbffb305a3715247c27cd8f
Md Rashed Hossain <wprashed@icloud.com>
2021-10-06 21:31:02 +0600

    Update class-elements-buddy-admin.php

--- admin/class-elements-buddy-admin.php
+++ admin/class-elements-buddy-admin.php
@@ -18,7 +18,7 @@
  *
  * @package    Elements_Buddy
  * @subpackage Elements_Buddy/admin
- * @author     iTech Theme <rashedcse18@gmail.com>
+ * @author     iTech Theme
  */
 class Elements_Buddy_Admin {
 
